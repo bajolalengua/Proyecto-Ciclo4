817335197660618771514218f940b2a623e7e21d
carlos <caerlosdavid29@gmail.com>
2021-11-08 20:51:44 -0500

    aplicacion empresa empeado grupo 10

---
+++ backendPedidos/src/controllers/index.ts
@@ -0,0 +1,7 @@
+export * from './ping.controller';
+export * from './empleado-empresa.controller';
+export * from './empresa-empleado.controller';
+export * from './empleado.controller';
+export * from './empresa.controller';
+export * from './empleado-empresa.controller';
+export * from './empresa-empleado.controller';
