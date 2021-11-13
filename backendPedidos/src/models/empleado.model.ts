4e7c8c2d30360f5d353d069fce29ebead0600d35
carlos <caerlosdavid29@gmail.com>
2021-11-13 09:36:02 -0500

    mejora clase empleado

--- backendPedidos/src/models/empleado.model.ts
+++ backendPedidos/src/models/empleado.model.ts
@@ -1,6 +1,6 @@
 import {Entity, model, property, belongsTo} from '@loopback/repository';
 import {Empresa} from './empresa.model';
-
+const twilio = require('twilio');
 @model()
 export class Empleado extends Entity {
   @property({
@@ -20,13 +20,62 @@ export class Empleado extends Entity {
     type: 'string',
     required: true,
   })
+  Apellidos: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
   telefono: string;
 
   @property({
     type: 'string',
     required: true,
   })
+  direccion: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  edad: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  fechaNacimiento: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  sueldo: number;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  esDirectivo: boolean;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  esCliente: boolean;
+  
+  @property({
+    type: 'string',
+    required: true,
+  })
   numeroDocumento: string;
+
 
   @belongsTo(() => Empresa)
   empresaId: string;
