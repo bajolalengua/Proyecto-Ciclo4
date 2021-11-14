ebfed5c4299bafdf5837a786e1436de4160b7346
Rocio Rojas Rincón <89947508+Rochi649@users.noreply.github.com>
2021-11-14 17:09:21 -0500

    Agregada cadena de conexion twilio Rocio

--- backendPedidos/src/services/notificacion.service.ts
+++ backendPedidos/src/services/notificacion.service.ts
@@ -10,9 +10,13 @@ export class NotificacionService {
   EnviarNotificacionesPorSMS():void
   {
     console.log("notificacion sms se agrego empleado");
-    
+/* conexion Carlos
     const accountSid = 'AC868c9e7ffb558bd592362c2f14170dd6'; // Your Account SID from www.twilio.com/console
     const authToken = 'c203d0803f723fe6ba5440d9a2fad418'; // Your Auth Token from www.twilio.com/console
+    */
+   // conexion Rocio
+    const accountSid = 'ACbbc256577ed0f4b412c4979802afd1bd'; // Your Account SID from www.twilio.com/console
+    const authToken = 'd334978e0a44ec66974a0bfbca6c9040'; // Your Auth Token from www.twilio.com/console
 
     const twilio = require('twilio');
     const client = new twilio(accountSid, authToken);
@@ -20,8 +24,13 @@ export class NotificacionService {
     client.messages
       .create({
         body: 'se agrego empleado',
+/*conexión carlos
         to: '+573225910556', // Text this number
         from: '+16672131003', // From a valid Twilio number
+*/
+//conexion Rocio
+        to: '+573222360608', // Text this number
+        from: '+18507357483', // From a valid Twilio number
       })
       .then((message:any) => console.log(message.sid));
 
